935f91b5728962ec8c450f4bfa120e381a9d4924
surya <surya.permual@arivu-iq.com>
2025-08-31 17:34:51 +0530

    removed unwanted var

--- src/SpeakingCharacter.js
+++ src/SpeakingCharacter.js
@@ -1,19 +1,4 @@
-// import React from "react";
-// import Lottie from "lottie-react";
-// import animationData from "./assets/keUUvukJnG.json";
 
-// const SpeakingCharacter = () => {
-//   return (
-//     <div className="avatar1" >
-//       <Lottie animationData={animationData} loop={true} className="imagesss"/>
-//     </div>
-//   );
-// };
-
-// export default SpeakingCharacter;
-
-
-// SpeakingCharacter.jsx
 import React, { useRef, useEffect } from "react";
 import Lottie from "lottie-react";
 import animationData from "./assets/keUUvukJnG.json";
